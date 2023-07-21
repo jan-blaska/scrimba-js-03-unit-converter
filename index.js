a3912c7186dcb08e96c53b03f50b0704b19993e3
Mr-Johny-Blaska <121683366+jan-blaska@users.noreply.github.com>
2023-07-21 19:17:50 +0200

    first commit of working app

--- index.js
+++ index.js
@@ -3,3 +3,44 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const convertBtn = document.getElementById("convert-btn");
+const inputField = document.getElementById("input-field");
+let lengthEl = document.getElementById("length");
+let volumeEl = document.getElementById("volume");
+let massEl = document.getElementById("mass");
+let inputNum = 0;
+let convertNum = 0;
+let unitArrMetric = ["meters", "liters", "kilos"];
+let unitArrIMperial = ["feet", "gallons", "pounds"];
+
+convertBtn.addEventListener("click", function() {
+    inputNum = inputField.value;
+    lengthEl.textContent = unitConverter(inputNum, "length");
+    volumeEl.textContent = unitConverter(inputNum, "volume");
+    massEl.textContent = unitConverter(inputNum, "mass");    
+});
+
+function unitConverter(inputNum, unit) {
+    let i = 0;
+    if (unit === "length") {
+        convertNum = 3.281;
+        i = 0;
+    } else if (unit === "volume") {
+        convertNum = 0.264;
+        i = 1;
+    } else {
+        convertNum = 2.204;
+        i = 2;
+    }
+    return (
+        `
+            ${inputNum} ${unitArrMetric[i]} = ${(inputNum * convertNum).toFixed(3)} ${unitArrIMperial[i]} 
+            | 
+            ${inputNum} ${unitArrIMperial[i] } = ${(1/convertNum * inputNum).toFixed(3)} ${unitArrMetric[i]}
+        `
+    )
+}
+
+
+
